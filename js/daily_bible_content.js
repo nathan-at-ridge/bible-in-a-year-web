19bc35959760c5ed89bf845646ce2bc318b5619f
nathan-at-ridge <121122977+nathan-at-ridge@users.noreply.github.com>
2023-01-21 10:08:47 -0800

    Update daily_bible_content.js

--- js/daily_bible_content.js
+++ js/daily_bible_content.js
@@ -22,7 +22,7 @@ let day = d.getDate();
 let month = d.toLocaleString('default', {month: 'long'})
 let year = d.getFullYear();
 document.getElementById("date").innerHTML = month.concat(" ", day, ", ", year);
-document.getElementById("bookmark").innerHTML = '<a href="'.concat(bookmark[month],'">',month," bookmark</a>");
+document.getElementById("bookmark").innerHTML = '<a href="'.concat(bookmark[month],'" target='_blank'>',month," bookmark</a>");
 //determine what day we're on in the reading plan
 let jan1 = new Date('01/01/2023');
 let day_in_plan = (d.getTime() - jan1.getTime()) / (1000 * 3600 * 24);
